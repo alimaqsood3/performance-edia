33ceeec5f8b0a0d79cd2b4db506cf7eb3844843c
alimaqsood3 <44001150+alimaqsood3@users.noreply.github.com>
2020-04-05 11:25:36 +0500

    Update style.css

--- public/css/style.css
+++ public/css/style.css
@@ -1,1 +1,95 @@
 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.img {
+  height: 100vh;
+  background: url("../images/Anonymous/0.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.form {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: 70px;
+  transform: translate(-50%, -50%);
+  width: 500px;
+  height: 300px;
+  background: rgba(0, 0, 0, 0.7);
+  border-radius: 70px;
+  overflow: hidden;
+}
+.form h1 {
+  font-size: 70px;
+  color: rgb(42, 146, 194);
+  padding: 10px;
+  width: 230px;
+  position: absolute;
+  top: 18%;
+  left: 31%;
+  text-align: center;
+  border-bottom: 2px solid #fff;
+  transition: all 0.7s;
+}
+form {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 280px;
+  text-align: center;
+}
+form .user input {
+  width: 90%;
+  background: transparent;
+  border: none;
+  padding: 8px 0;
+  float: left;
+  font-size: 18px;
+  border-bottom: 1px solid #fff;
+  outline: none;
+
+  transition: all 0.2s;
+  color: rgb(155, 3, 3);
+  font-size: 20px;
+}
+.user {
+  margin: 35px 0;
+}
+i {
+  float: left;
+  width: 24px;
+  font-size: 18px;
+  margin-top: 7px;
+  color: rgb(192, 33, 33);
+  transition: all 0.7s;
+}
+.example_e {
+  border: none;
+  background: #141414;
+  color: #ee1414 !important;
+  font-weight: 100;
+  padding: 20px;
+  text-transform: uppercase;
+  border-radius: 6px;
+  margin-top: 20px;
+  display: inline-block;
+  transition: all 0.3s ease 0s;
+  }
+
+
+
+  .example_e:hover {
+    color: red !important;
+    font-weight: 700 !important;
+    letter-spacing: 3px;
+    background: none;
+    -webkit-box-shadow: 0px 5px 40px -10px rgba(250, 2, 2, 0.57);
+    -moz-box-shadow: 0px 5px 40px -10px rgba(226, 27, 27, 0.57);
+   
+    }
+
